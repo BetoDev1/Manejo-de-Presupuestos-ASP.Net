b1b012c96c12439dc150f789a9d3ab24f4ef9e8f
Norberto Caballero <norbertocaballero25@gmail.com>
2022-06-27 10:29:31 -0300

    first

---
+++ ManejoPresupuesto/Models/TipoCuentaModel.cs
@@ -0,0 +1,38 @@
+﻿
+
+using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuentaModel
+    {
+        public int Id { get; set; }        
+        [Required(ErrorMessage ="El campo Nombre es obligatorio")]
+        [PrimeraLetraMayuscula]
+        [Remote(action:"VerificarExisteTipoCuenta", controller:"TiposCuentas")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+
+        //pruebas de validaciones
+        //[EmailAddress(ErrorMessage ="El campo debe ser un correo")]
+        //public string Email { get; set; }
+
+        //[Range(minimum: 18, maximum:130, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        //public int Edad { get; set; }
+
+        //[Url(ErrorMessage ="El campo debe ser una direccion de internet")]
+        //public string URL { get; set; }
+
+        //[CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        //public string TarjetaDeCredito { get; set; }
+
+
+
+
+
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Servicios
+{
+
+    public interface IRepositorioTiposCuentas
+    {
+        Task Actualizar(TipoCuentaModel tipoCuenta);
+        Task Crear(TipoCuentaModel tipocuenta);
+        Task<bool> Existe(string Nombre, int UsuarioId);
+        Task<IEnumerable<TipoCuentaModel>> Obtener(int usuarioId);
+        Task<TipoCuentaModel> ObtenerPorId(int id, int usuarioId);
+    }
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Models.TipoCuentaModel tipocuenta)
+        {
+            using var connection = new SqlConnection(connectionString) ;
+            var id = await connection.QuerySingleAsync<int>($@"insert into TiposCuentas (Nombre, UsuarioId, Orden)
+                                                 values (@Nombre, @UsuarioId, 0);
+                                                    SELECT SCOPE_IDENTITY()", tipocuenta);
+            tipocuenta.Id = id;
+        }
+
+        public async Task<bool> Existe(string Nombre, int UsuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(@"select 1 from TiposCuentas where Nombre = @Nombre
+                                                                           AND UsuarioID = @UsuarioId;", new { Nombre, UsuarioId });
+            return existe == 1;
+
+        }
+
+        public async Task<IEnumerable<TipoCuentaModel>>Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            return await connection.QueryAsync<TipoCuentaModel>($"Select Nombre, UsuarioId, Orden " +
+                $"FROM TiposCuentas WHERE UsuarioId = @UsuarioId",
+                new { usuarioId });
+
+        }
+
+        public async Task Actualizar(TipoCuentaModel tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE TiposCuentas SET Nombre = @Nombre WHERE Id = @Id", tipoCuenta);
+        }
+
+        public async Task<Models.TipoCuentaModel>ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoCuentaModel>(@"
+                                                                SELECT Id, Nombre, Orden
+                                                                FROM TiposCuentas
+                                                                WHERE Id = @Id AND UsuarioId = @UsuarioId",
+                                                                new { id, usuarioId });
+        }
+
+    }
+}
